4387619d7ffdcd1fe3709e67cf5060a36dc48611
Rachit <rachitsinghal99@gmail.com>
2018-06-14 21:12:13 +0530

    First Commit

---
+++ app/src/main/java/com/example/rachitsinghal/flickrfeed/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.rachitsinghal.flickrfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView= (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new Adapter());
+    }
+}
